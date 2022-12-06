d6f20bcac3f7bb3ede47c9f57268a2ddcd4e8be4
Phil Barton <Phil.Barton@bjss.com>
2022-12-06 10:22:36 +0000

    day 6 - rework

--- day6/script.py
+++ day6/script.py
@@ -1,25 +1,14 @@
 def get_chunk(transmission, chunk_size):
     for x in range(0, len(transmission)+1):
-        chunk = transmission[x:x+chunk_size]
-        x += 1
-        yield (chunk, (x+chunk_size-1))
+        yield (transmission[x:x+chunk_size], (x+chunk_size))
 
-# part 1
-with open('input.txt', 'r') as input:
-    transmission = input.read()
-    chunk = get_chunk(transmission, 4)
-    while x := next(chunk):
-        data, count = x
-        if(len(set(data)) == len(data)):
-            print(data, count)
-            break
+def get_answer(input, size):
+    return ([x for x in get_chunk(input, size) if len(set(x[0])) == len(x[0])][0][1])
 
-# part 2
 with open('input.txt', 'r') as input:
-    transmission = input.read()
-    chunk = get_chunk(transmission, 14)
-    while x := next(chunk):
-        data, count = x
-        if(len(set(data)) == len(data)):
-            print(data, count)
-            break
+    # part 1
+    print(get_answer(input.read(), 4))
+    input.seek(0, 0)
+
+    # part 2
+    print(get_answer(input.read(), 14))
